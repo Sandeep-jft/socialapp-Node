4268d6ddd492cb5f2530a07950a6de37e6e1f19b
sandeep-jft <sandeep.singh@jellyfishtechnologies.com>
2020-08-06 13:30:02 +0530

    check code

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var app = express();
 var mongoose = require("mongoose");
 var { MONGOURI } = require("./config/keys");
 var PORT = process.env.PORT || 5000;
+var cors = require("cors");
 
 mongoose.connect(MONGOURI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.connection.on("connected", () => {
@@ -13,6 +14,8 @@ mongoose.connection.on("error", (err) => {
   console.log("Error : ", err);
 });
 
+app.use(cors());
+
 require("./models/user");
 require("./models/post");
 
@@ -22,15 +25,15 @@ app.use(require("./routes/r-auth"));
 app.use(require("./routes/r-post"));
 app.use(require("./routes/r-user"));
 
-if ((process.env.NODE_ENV = "production")) {
-  app.use(express.static("mysocialappfront/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "mysocialappfront", "build", "index.html")
-    );
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("mysocialappfront/build"));
+//   const path = require("path");
+//   app.get("*", (req, res) => {
+//     res.sendFile(
+//       path.resolve(__dirname, "mysocialappfront", "build", "index.html")
+//     );
+//   });
+// }
 
 app.listen(PORT, () => {
   console.log("server is running at port 5000");
